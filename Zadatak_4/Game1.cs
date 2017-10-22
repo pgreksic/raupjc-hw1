82ded43e08cf766632cfc77c0d3f08422e7d4bb9
pgreksic <petar.greksic@fer.hr>
2017-10-23 01:53:18 +0200

    Zadatak_4 update

--- Zadatak_4/Game1.cs
+++ Zadatak_4/Game1.cs
@@ -80,4 +80,61 @@ namespace Zadatak_4
             base.Draw(gameTime);
         }
     }
+
+    public class Background : Sprite
+    {
+        public Background(int width, int height) : base(width, height)
+        {
+
+        }
+    }
+
+    public class Ball : Sprite
+    {
+        /// <summary >
+        /// Defines current ball speed in time .
+        /// </ summary >
+        public float Speed { get; set; }
+        public float BumpSpeedIncreaseFactor { get; set; }
+        /// <summary >
+        /// Defines ball direction .
+        /// Valid values ( -1 , -1) , (1 ,1) , (1 , -1) , ( -1 ,1).
+        /// Using Vector2 to simplify game calculation . Potentially
+        /// dangerous because vector 2 can swallow other values as well .
+        /// OPTIONAL TODO : create your own , more suitable type
+        /// </ summary >
+        public Vector2 Direction { get; set; }
+        public Ball(int size, float speed, float
+            defaultBallBumpSpeedIncreaseFactor) : base(size, size)
+        {
+            Speed = speed;
+            BumpSpeedIncreaseFactor = defaultBallBumpSpeedIncreaseFactor;
+            // Initial direction
+            Direction = new Vector2(1, 1);
+        }
+    }
+
+    /// <summary >
+    /// Represents player paddle .
+    /// </ summary >
+    public class Paddle : Sprite
+    {
+        /// <summary >
+        /// Current paddle speed in time
+        /// </ summary >
+        public float Speed { get; set; }
+        public Paddle(int width, int height, float initialSpeed) : base(width,
+            height)
+        {
+            Speed = initialSpeed;
+        }
+        /// <summary >
+        /// Overriding draw method . Masking paddle texture with black color .
+        /// </ summary >
+        public override void DrawSpriteOnScreen(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(Texture, new Vector2(X, Y), new Rectangle(0, 0,
+                Width, Height), Color.GhostWhite);
+        }
+    }
 }
